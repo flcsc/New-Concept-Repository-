0cd7937ee097d2ae9a8f29afb72c39e2e325a54b
Clafted <noahm.p.in2017@gmail.com>
2023-11-08 08:01:04 -0500

    Added Noah's class

---
+++ Noahs.java
@@ -0,0 +1,8 @@
+public class Noahs 
+{
+    // The method is "static", meaning that it has the same value for all Noahs instances
+    public static void sayHiToNoah()
+    {
+        System.out.println("Hi, Noah!");
+    }
+}
