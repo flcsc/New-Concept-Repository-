c5077ed8caa4e02b208644b307d0919cf6eb3809
raiderblast <149011186+raiderblast@users.noreply.github.com>
2023-11-08 15:35:47 -0500

    Update Noahs.java

--- Noahs.java
+++ Noahs.java
@@ -1,7 +1,7 @@
 public class Noahs 
 {
     // The method is "static", meaning that it has the same value for all Noahs instances
-    public static void sayHiToNoah()
+    public static void sayHiEmmanuel()
     {
         System.out.println("Hi, Noah!");
     }
