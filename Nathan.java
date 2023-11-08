bde81b999c3bcfbbb1e7094bef125e966e25bb3d
Nathan Dias <148722682+NateTheMate14@users.noreply.github.com>
2023-11-08 15:19:06 -0500

    Create Nathan.java

---
+++ Nathan.java
@@ -0,0 +1,14 @@
+public class Nathan
+{
+	// Prints "Hello Nathan"
+	public void sayHello()
+	{
+		System.out.println("Hello Nathan");
+	}
+
+	// Says "How are you" to user
+	public void howAreYou(String name)
+	{
+		System.out.println("How are you, " + name + "?");
+	}
+}
