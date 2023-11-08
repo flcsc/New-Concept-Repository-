99ee3aaf530abf04d01094d2053869d7c3a888fb
gkNoel <148814372+gkNoel@users.noreply.github.com>
2023-11-08 15:12:51 -0500

    Create Noel.java

---
+++ Noel.java
@@ -0,0 +1,8 @@
+public class Noel 
+{
+    // The method is "static", meaning that it has the same value for all Noel instances
+    public static void sayHiToMe()
+    {
+        System.out.println("Hi, Noel");
+    }
+}
