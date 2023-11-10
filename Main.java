2bfa20cbc0d2e850078436079547d380c3efcbc2
Noah Perez <noahm.p.in2017@gmail.com>
2023-11-10 17:02:27 -0500

    Added javadoc comments to Main.java

--- Main.java
+++ Main.java
@@ -8,20 +8,27 @@ class Main{
         String [] classNames = {"Noahs", "AlexTran", "Emmanueli", "Evan", "Fina's", "Nathan", "Noel", "Rafaels", "Teghan"};
         Class<?>[] classes = new Class<?>[classNames.length];
 
+        classNames = sortAlphabetically(classNames);
+
+        // Add all existing classes to classes variable from classNames
         for(int i = 0; i < classes.length; i++){
 
             try{
+
                 classes[i] = Class.forName(classNames[i]);
             }catch(ClassNotFoundException e){
+
                 System.out.println("Cannot find class: " + classNames[i] + ".java");
                 classes[i] = null;
-            }
-            
+            } 
         }
-
         printClassInformation(classes);
     }
 
+    /**
+     * Prints information of Class Objects, specifically the class name and its methods in a numbered list.
+     * @param classes the Class array to be displayed
+     */
     private static void printClassInformation(Class<?>[] classes){
 
         Method[] classMethods;
@@ -44,5 +51,31 @@ class Main{
                 System.out.println((i+1) + ". " + classMethods[i]);
         
         }    
+    }
+    
+    /**
+     * Takes an array of Strings and sorts them in lexicographical order, from least to greatest (A-Z).
+     * 
+     * @param array the String array to be sorted
+     * @return the sorted String array
+     */
+    private static String[] sortAlphabetically(String[] array){
+        String sorted[] = array.clone();
+        int lowestValueIndex;
+        String temp;
+
+        for(int i = 0; i < sorted.length; i++){
+
+            lowestValueIndex = i;
+            temp = sorted[i];
+            for(int j = i; j < sorted.length; j++){
+
+                if(sorted[j].compareTo(sorted[lowestValueIndex]) < 1)
+                    lowestValueIndex = j;
+            }
+            sorted[i] = sorted[lowestValueIndex];
+            sorted[lowestValueIndex] = temp;
+        }
+        return sorted;
     }
 }
