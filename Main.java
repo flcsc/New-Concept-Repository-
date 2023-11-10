2d73a1761f56c0d30b58ccad792d279ad8a78a0d
Noah Perez <noahm.p.in2017@gmail.com>
2023-11-10 13:23:34 -0500

    Added reflection functionality to Main class

--- Main.java
+++ Main.java
@@ -1,12 +1,38 @@
-class Main
-{
-    public static void main(String[] args)
-    {
-        System.out.println("Hello World!");
-        Noahs.sayHiNoah();
-        Noel.sayHiToMe();
-        Rafaels.sayHiToRaf();
-        Teghan.SayHiToTeghan();
+import java.lang.reflect.*;
+import java.util.ArrayList;
 
+class Main{
+
+    public static void main(String[] args){
+
+        String [] classNames = {"Noahs", "AlexTran", "Emmanueli", "Evan", "EvanRaju", "Fina's", "Nathan", "Noel", "Rafaels", "Teghan"};
+        Method[] classMethods;
+        ArrayList<Method> allMethods = new ArrayList<>();
+
+        for(String name : classNames){
+
+            try{
+
+                classMethods = Class.forName(name).getDeclaredMethods();
+                for(Method method : classMethods)
+                    allMethods.add(method);
+            }catch(ClassNotFoundException e){
+                
+                e.printStackTrace();
+            }
+
+        }
+
+        for(Method method : allMethods){
+
+            try{
+                method.invoke(method.getClass());
+            }catch(Exception e){
+                e.printStackTrace();
+            }
+            System.out.println();
+        }
+
+        System.out.println("Hello World!");
     }
 }
