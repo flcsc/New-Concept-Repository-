e5a0b627bd35b7540731e2200f84c67ae6efe3f1
PINTOGavin <pintog001@tcdsb.ca>
2023-11-22 15:47:17 -0500

    Nov. 22 lesson

---
+++ DataTypes.java
@@ -0,0 +1,54 @@
+public class DataTypes {
+    
+    public static void main(String[] args) {
+
+        System.out.println("Hello World");
+
+        int a = 1;
+        boolean b = true;
+        float aaa = 3.24f;
+
+
+        // modulus: a % b 
+        int[] scores = {90, 88, 93, 79};
+        // Index        0   1    2   3
+
+        for(int i = 0; i < scores.length; i++) {
+
+            int lastDigit = scores[i] % 10;
+
+            if (lastDigit >= 5) {
+
+                scores[i] += (10 - lastDigit);
+                
+            }
+
+            System.out.println(scores[i]);
+
+        }
+
+        
+
+
+        int[] ages = {3, 12, 19, 4, 10};
+        ages[2] = 7;
+
+        for (int i = 0; i <= 4; i++) {
+            System.out.print("x");
+        }
+
+
+
+
+        
+
+
+
+
+
+
+
+
+    }
+
+}
