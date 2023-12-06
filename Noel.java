bebea595a4ff6ceab75527ab230f0167fda195b3
gkNoel <noelgk@hotmail.com>
2023-12-06 15:39:54 -0500

    Added two constructors, getter and setter methods and a toString to class Noel

--- Noel.java
+++ Noel.java
@@ -2,6 +2,61 @@ import java.util.Scanner;
 
 public class Noel
 {
+    private String name;
+    private int age;
+    private String game; 
+    private int hrs;
+    
+    public Noel(){
+        this.name="Noel";
+        this.age=16;
+        this.game="Sonic Frontiers";
+        this.hrs=100;
+    }
+
+    public Noel(String name, int age, String game, int hrs){
+        this.name=name;
+        this.age=age;
+        this.game=game;
+        this.hrs=hrs;
+    }
+
+    public String getName(){
+        return name;
+    }
+    
+    public void setName(String name){
+        this.name=name;
+    }
+
+    public int getAge(){
+        return age;
+    }
+
+    public void setAge(int age){
+        this.age=age;
+    }
+
+    public String getGame(){
+        return game;
+    }
+
+    public void setGame(String game){
+        this.game=game;
+    }
+
+    public int getHours(){
+        return hrs;
+    }
+
+    public void setHours(int hrs){
+        this.hrs=hrs;
+    }
+
+    public String toString(){
+        return name+" is "+age+" and has played "+game+" for "+hrs+" hours. ";
+    }
+
     // The method is "static", meaning that it has the same value for all Noel instances
     public static void sayHiToMe()
     {
@@ -12,4 +67,5 @@ public class Noel
         System.out.println("Hi there "+name);
         input.close(); // MAKE SURE TO CLOSE THE SCANNER
     }
+    
 }
