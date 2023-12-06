b8e4ef56d1edc9d9771576159b17928e3935594a
PINTOGavin <pintog001@tcdsb.ca>
2023-12-06 17:32:01 -0500

    Added two classes to show how OOP works

---
+++ GavinCreator.java
@@ -0,0 +1,12 @@
+public class GavinCreator {
+    
+    public static void main(String[] args) {
+
+        Gavin g = new Gavin(17, 180);
+
+        g.watchMovie("Home Alone");
+        System.out.println(g);
+
+    }
+
+}
